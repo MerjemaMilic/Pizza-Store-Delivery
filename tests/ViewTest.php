8064710e1f936134cab2079233ba38ab5a69efea
idabajrami <39003040+pitagorinaunuka@users.noreply.github.com>
2020-06-01 00:26:51 +0200

    PHPUnit test

---
+++ tests/ViewTest.php
@@ -0,0 +1,36 @@
+<?php declare (strict_types = 1);
+
+use PHPUnit\Framework\TestCase;
+
+class ViewTest extends TestCase
+{
+    /**
+     * @var \flight\template\View
+     */
+    private $view;
+
+    public function setUp()
+    {
+        $this->view = new \flight\template\View();
+        $this->view->path = 'views';
+    }
+
+    public function testTemplateExists()
+    {
+        $this->assertTrue($this->view->exists('menu.php'));
+        $this->assertTrue($this->view->exists('homepage.php'));
+        $this->assertTrue($this->view->exists('addCategory.php'));
+        $this->assertTrue($this->view->exists('addFood.php'));
+        $this->assertTrue($this->view->exists('editFood.php'));
+        $this->assertTrue($this->view->exists('messageView.php'));
+        $this->assertTrue($this->view->exists('panel.php'));
+        $this->assertTrue($this->view->exists('showFood.php'));
+        $this->assertFalse($this->view->exists('homepage5463.php'));
+        $this->assertFalse($this->view->exists('servies.html'));
+        $this->assertFalse($this->view->exists('menu.html'));
+        $this->assertFalse($this->view->exists('contact.html'));
+        $this->assertFalse($this->view->exists('blog.html'));
+
+    }
+
+}
